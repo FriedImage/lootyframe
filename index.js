e651439d70e7511ca9e2bf18ba782ac459ed1899
George Ntolias <ntoliasg@gmail.com>
2024-03-24 20:07:52 +0200

    + filesystem search for commands .js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 // Bot Accesses
 const client = new Client({
@@ -15,5 +17,35 @@ client.once(Events.ClientReady, readyClient => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-// bot login
+// Bot login
 client.login(process.env.DISCORD_TOKEN);
+
+// Collection of commands
+client.commands = new Collection();
+
+// Find commands folder
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+// Find all command files (only .js)
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Listen commands
+client.on(Events.InteractionCreate, interaction => {
+	console.log(interaction);
+});
