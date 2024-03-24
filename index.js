f428133cd7e2d859c3a0d3a8f57f5369e86ed5f2
George Ntolias <ntoliasg@gmail.com>
2024-03-24 17:46:13 +0200

    ~ change name to index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// accesses
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.DirectMessages,
+	],
+});
+
+// bot login
+client.login(process.env.DISCORD_TOKEN);
