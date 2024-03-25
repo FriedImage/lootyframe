8fca2df3574ed4930839a1914c66a98f95121a9b
George Ntolias <ntoliasg@gmail.com>
2024-03-25 02:42:31 +0200

    ~ add process.env back to token

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
 
 // Bot Accesses
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -52,4 +51,4 @@ for (const file of eventFiles) {
 }
 
 // Bot login
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
